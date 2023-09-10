a40723e49988d5704e22e33da61134963e1c13b5
kashishInamdar <kashishinamdar2004@gmail.com>
2023-09-10 23:58:12 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const project=document.getElementById('project');
+
+function card(){
+    {
+        img : '',
+        heading : '',
+        contain : '',
+        demo : '',
+        sCode : '',
+    },
+    {
+        img : '',
+        heading : '',
+        contain : '',
+        demo : '',
+        sCode : '',
+    },
+    {
+        img : '',
+        heading : '',
+        contain : '',
+        demo : '',
+        sCode : '',
+    },
+
+
+}
