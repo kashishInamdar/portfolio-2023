a532ca846121b5937438b8485d2b69541ac9a1e2
kashishInamdar <kashishinamdar2004@gmail.com>
2023-09-12 05:20:28 +0530

    add Project Page

--- index.js
+++ index.js
@@ -1,27 +1,46 @@
-const project=document.getElementById('project');
 
-function card(){
-    {
-        img : '',
-        heading : '',
-        contain : '',
-        demo : '',
-        sCode : '',
-    },
-    {
-        img : '',
-        heading : '',
-        contain : '',
-        demo : '',
-        sCode : '',
-    },
-    {
-        img : '',
-        heading : '',
-        contain : '',
-        demo : '',
-        sCode : '',
-    },
+// const projet = [
+//     {
+//         imgUrl : "image/project/smartdealsmart.png",
+//         name : "Smart Deals Mart",
+//         content : "",
+//         demo : "https://dmart-igcp.netlify.app/",
+//         sCode : "https://github.com/kashishInamdar/DMart-IGCP-Second-group-project",
+//     },
+//     {
+//         imgUrl : "image/project/theGrandUnison.png",
+//         name : "The Grand Unison",
+//         content : " ",
+//         demo : " https://the-grand-unison.netlify.app/",
+//         sCode : " https://github.com/kashishInamdar/The-Grand-Unison-IGCP1-first-group-project",
+//     },
+//     {
+//         imgUrl : "image/project/workOnJS.png",
+//         name : "Work On JS",
+//         content : " ",
+//         demo : " https://igcp-projec3.netlify.app/",
+//         sCode : "https://github.com/kashishInamdar/igcp-project3-of-js ",
+//     },
+// ]
+// const card = document.getElementById('project')
+//  for(let i=0 ;i < projet.length ; i++){
+//     let {ImgUrl, name , content ,demo, sCode } = projet[i];
 
+//     card.innerHTML= card.innerHTML + `
+    
+//     <div class="col ">
+//                 <div class="card">
+//                     <img src=${ImgUrl} class="projectImg" />
+//                     <h1 class="heading">${name}</h1>
+//                     <p>
+//                         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit quisquam atque, culpa minima adipisci sed accusantium accusamus distinctio dolorem blanditiis.
+//                     </p>
+//                     <div class="d-flex justify-content-evenly">
+//                     <a href=" ${sCode}"><button type="button" class="btn btn-outline-danger">Source Code </button></a>
+//                      <a href="${demo} "><button type="button" class="btn btn-outline-info">Live Demo </button></a>
+//                     </div>
+//                 </div>
+//             </div>
+//     `
 
-}
+//  }
